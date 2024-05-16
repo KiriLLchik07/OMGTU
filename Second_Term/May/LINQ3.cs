b48f3e3d5fd1dfcc9ff66cdfd48b68d54db37d6a
KiriLLchik07 <144825849+KiriLLchik07@users.noreply.github.com>
2024-05-16 09:07:13 +0600

    Update LINQ3.cs

--- Second_Term/May/LINQ3.cs
+++ Second_Term/May/LINQ3.cs
@@ -12,6 +12,7 @@ class Program
         int[] numbers = { 123, 456, 789, 111, 415, 800, 444, 555, 666, 1234561, 0, 112 };
         var result = numbers.Where(n => n.ToString().Select(c => int.Parse(c.ToString())).Sum() % 2 == 0);
 
+        Console.WriteLine("Элементы с четной суммой цифр:\n ");
         foreach (var res in result)
         {
             Console.WriteLine(res);
