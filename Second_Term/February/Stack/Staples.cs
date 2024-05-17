4e2e386409b7c80d0423f02c030a4617129bb8be
KiriLLchik07 <144825849+KiriLLchik07@users.noreply.github.com>
2024-05-17 21:33:54 +0600

    Update Staples.cs

--- Second_Term/February/Stack/Staples.cs
+++ Second_Term/February/Stack/Staples.cs
@@ -2,24 +2,41 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введите выражение, содержащее 3 вида скобок: () {} []: ");
+        Console.WriteLine("Введите строку со скобками:");
         string str = Console.ReadLine();
-        char[] staples = new char [] { '(', ')', '[', ']', '{', '}' };
+        char[] parens = new char[] { '(', ')', '[', ']', '{', '}' };
 
         Stack<char> stack = new Stack<char>();
 
-        foreach(char c in str)
+        foreach (char c in str)
         {
-            int index = Array.IndexOf(staples, c);
-            if(c == '(' || c == '[' || c == '{') stack.Push(c);
+            int index = Array.IndexOf(parens, c);
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
             else if (index != -1)
             {
-                if (stack.Count > 0 && stack.Peek() == staples[index-1]) stack.Pop();
-            }   else stack.Push(c);
+                if (stack.Count > 0 && stack.Peek() == parens[index - 1])
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
         }
 
-        if (stack.Count > 0) Console.WriteLine("Неверно расставлены скобки");
-        else Console.WriteLine("Скобки расставлены верно");
+        Console.WriteLine();
 
+        if (stack.Count > 0)
+        {
+            Console.WriteLine("Неверно расставленны скобки!");
+        }
+        else
+        {
+            Console.WriteLine("Скобки расставлены верно!");
+        }
     }
 }
