c83d718e0d58932541fa4a8961619f79b3e4e604
KiriLLchik07 <144825849+KiriLLchik07@users.noreply.github.com>
2024-05-21 14:11:10 +0600

    Create Алгоритм Робертса-Флореса.py

---
+++ Дискретная математика/Алгоритм Робертса-Флореса.py
@@ -0,0 +1,40 @@
+def hamiltonian_cycle_util(graph, path, pos):
+    if pos == len(graph) and graph[path[pos-1]][path[0]] == 1:
+        return True
+
+    for v in range(1, len(graph)):
+        if is_valid(v, pos, path, graph):
+            path[pos] = v
+
+            if hamiltonian_cycle_util(graph, path, pos+1):
+                return True
+
+            path[pos] = -1
+
+    return False
+
+def hamiltonian_cycle(graph):
+    n = len(graph)
+    path = [-1] * n
+
+    path[0] = 0
+
+    if not hamiltonian_cycle_util(graph, path, 1):
+        print("\nГамильтонов цикл не существует.\n")
+        return False
+
+    print("\nГамильтонов цикл существует:\n")
+    print(path)
+    return True
+
+graph = [
+    [0, 1, 1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1, 1],
+    [0, 1, 1, 1, 0, 1, 0],
+    [0, 0, 1, 1, 1, 0, 1],
+    [0, 1, 1, 1, 0, 1, 0]
+]
+
+hamiltonian_cycle(graph)
